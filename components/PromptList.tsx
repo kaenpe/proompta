7b0c118a3840cbcb462750757b4d08947469690c
knapalke <knapalke@gmail.com>
2023-05-11 12:31:10 +0200

    starting feed

--- components/PromptList.tsx
+++ components/PromptList.tsx
@@ -2,13 +2,7 @@ import React from "react";
 import Card from "./Card";
 
 const PromptList = ({ data, handleTagClick }) => {
-	return (
-		<div className="mt-16 prompt_layout">
-			{data.map((el, id) => (
-				<Card key={id} prompt={el.prompt}></Card>
-			))}
-		</div>
-	);
+	return <div className="mt-16 prompt_layout">Yo</div>;
 };
 
 export default PromptList;
